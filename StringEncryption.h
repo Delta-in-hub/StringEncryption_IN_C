06402eeafd519541c31f27238ac63339d9bbd70d
Delta-in-hub <53047182+Delta-in-hub@users.noreply.github.com>
2020-05-19 18:03:21 +0800

    Update StringEncryption.h

--- StringEncryption.h
+++ StringEncryption.h
@@ -1,1 +1,10 @@
+#ifndef StringEncryption
+#define StringEncryption
 
+#define MAX_BUF 256
+
+void EncryptString(const unsigned char *string, int *code);
+
+void DecryptString(const int *code, char *string);
+
+#endif
